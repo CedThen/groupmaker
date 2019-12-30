11ccd6fc484c7265a6814aef51a8c4f050a31ea0
frawg.lehgs@gmail.com <frawg.lehgs@gmail.com>
2019-12-30 10:57:02 -0500

    lots of changes

--- groups api/server.js
+++ groups api/server.js
@@ -6,6 +6,19 @@ app.use(express.json());
 const wss = new WebSocket.Server({ port: 3030 });
 
 let currentConnectedNames = [];
+let connectedSockets = [];
+
+class Clients {
+  constructor() {
+    this.clientList = {};
+    this.saveClient = this.saveClient.bind(this);
+  }
+  saveClient(username, client) {
+    this.clientList[username] = client;
+  }
+}
+
+const clients = new Clients();
 
 wss.on("connection", ws => {
   wss.clients.forEach(distributeNamesToClient);
@@ -16,10 +29,17 @@ wss.on("connection", ws => {
 
     switch (message.type) {
       case "addname":
+        let socket = {
+          socket: ws,
+          name: message.payload
+        };
+        connectedSockets.push(socket); //remove
+        clients.saveClient(message.payload, ws);
         currentConnectedNames.push(message.payload);
         wss.clients.forEach(distributeNamesToClient);
         break;
       case "splitgroups":
+        console.log("splitting");
         splitIntoGroups(message.payload);
         break;
       default:
@@ -29,11 +49,61 @@ wss.on("connection", ws => {
 });
 
 function distributeNamesToClient(client) {
+  let message = {
+    type: "namearray",
+    payload: currentConnectedNames
+  };
   if (client.readyState === WebSocket.OPEN) {
-    client.send(JSON.stringify(currentConnectedNames));
+    client.send(JSON.stringify(message));
   }
 }
 
 function splitIntoGroups({ groupSize, numGroups }) {
-  console.log(groupSize, numGroups);
+  let shuffledNames = shuffle(currentConnectedNames);
+  const splitNames = split(shuffledNames, groupSize, numGroups);
+  distributeGroups(splitNames);
+  connectedSockets.map(connection => {
+    connection.socket.send(JSON.stringify("haha"));
+  });
+}
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function split(namesArray, groupSize, numGroups) {
+  let a = [];
+  namesArray.map((name, index) => {
+    if (index % groupSize === 0) {
+      a.push([name]);
+    } else {
+      a[a.length - 1].push(name);
+    }
+  });
+  return a;
+}
+
+function distributeGroups(groupedNames) {
+  groupedNames.map(group => {
+    group.map(name => {
+      let message = {
+        type: "group",
+        payload: group
+      };
+      //connection.socket.send(JSON.stringify(message));
+      clients.clientList[name].send(JSON.stringify(message));
+      console.log("awel;fjasd");
+    });
+  });
 }
